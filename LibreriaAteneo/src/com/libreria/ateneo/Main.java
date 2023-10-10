23cd77e481d5013ba0be4e10a5bb822237208e15
CarlaHassan <ca.hassan@duocuc.cl>
2023-10-10 10:23:53 -0300

    custumer creados

--- LibreriaAteneo/src/com/libreria/ateneo/Main.java
+++ LibreriaAteneo/src/com/libreria/ateneo/Main.java
@@ -7,6 +7,23 @@ package com.libreria.ateneo;
  */
 public class Main {
     public static void main(String[] args) {
+        Libro harry = new Libro("Harry Potter", "jkl", 19000, "12345678");
         
+        System.out.println(harry.getTitulo());
+        
+        harry.setTitulo("Harry Potter y la piedra filosofal");
+        
+        System.out.println(harry.getTitulo());
+        
+        harry.imprimirInfo();
+        System.out.println(harry.calcularIva());
+        harry.ajustarPrecio();
+        harry.imprimirInfo();
+        
+        System.out.println(harry.toString());
+        
+        
+        
+          
     }
 }
